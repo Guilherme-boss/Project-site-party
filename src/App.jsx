64f58886926e53dc24231b8c38a3e850a926f0c7
Guilherme-Boss <guips4samu@gmail.com>
2025-03-11 23:36:09 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; 
-import Services from "./pages/services";
+import Services from "./pages/Services.jsx";
 import Header from "./components/Header";
 import Contato from "./pages/Contato";
 import Rodape from "./components/Rodape";
